99831afbba08e1bdb6552610569494e904e911dc
disco <stickman5464@gmail.com>
2023-05-30 17:02:14 +1000

    ..

--- Flask_Files/app.py
+++ Flask_Files/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.secret_key = "temp_key"
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = "5fwja35"
@@ -12,26 +13,55 @@ mysql = MySQL(app)
 @app.route('/', methods =['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        name = request.form['customerName']
-        email = request.form['email']
-        phone = request.form['phone']
-        outID = -1;
+        try:
+            name = request.form['customerName']
+            email = request.form['email']
+            phone = request.form['phone']
+            outID = -1;
 
-        cursor = mysql.connection.cursor()
-        cursor.callproc('stdAddUser', (name, email, phone, outID))
-        #outputN = cursor.stored_results()
-        mysql.connection.commit()
-        cursor.close()
+            cursor = mysql.connection.cursor()
+            cursor.callproc('stdAddUser', (name, email, phone, outID))
+            mysql.connection.commit()
+            cursor.close()
+        except:
+            flash("Error: Unable to submit new entry, not connected to SQL database")
     return render_template('home.html')
 
-@app.route('/customer_details')
+@app.route('/customer_details', methods = ['GET', 'POST'])
 def customer_details():
-    cur = mysql.connection.cursor()
-    customers = cur.execute("SELECT * FROM customer_table")
+    connection = False #validation for sql connection
+    try:
+        cur = mysql.connection.cursor()
+        customers = cur.execute("SELECT * FROM customer_table")
+        connection = True
+        if customers > 0:
+            customer_details = cur.fetchall()
+            return render_template('customers.html', customerDetails = customer_details, connected = connection)
+    except:
+        flash("Error: Database unavailable. Not connected to MySQL Database Server")
+        return render_template('customers.html', connected = connection)
+    if request.method == 'POST':
+        try:
+            name = request.form['customerName']
+            email = request.form['email']
+            phone = request.form['phone']
+            outID = -1;
 
-    if customers > 0:
-        customer_details = cur.fetchall()
-        return render_template('customers.html', customerDetails = customer_details)
+            cursor = mysql.connection.cursor()
+            cursor.callproc('stdAddUser', (name, email, phone, outID))
+            mysql.connection.commit()
+            cursor.close()
+        except:
+            flash("Error: Unable to submit new entry, not connected to SQL database")
+
+
+@app.route('/page_3')
+def page_3():
+    return render_template('page3.html')
+
+@app.route('/page_4')
+def page_4():
+    return render_template('page4.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
